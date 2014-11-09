3c0b5272084bfd5779fab84c636714e5a941aa61
Ahmed Sami <asyn@gmail.com>
2014-11-09 11:26:43 +0200

    Sidebar Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
-  def index
-  	@products = Product.order(:title)
-  end
+	include CurrentCart
+	before_action :set_cart
+
+	def index
+		@products = Product.order(:title)
+	end
 end
